8084638652f0d23aa83efa3ae96407093d3bad73
Madhusmita113 <madhubiswal113@gmail.com>
2024-07-21 21:31:55 +0530

    Adding fixtures

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,72 +1,54 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
+import pytest
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestSearch:
+    def test_login_with_valid_credentials(self):
+        self.driver.find_element(By.XPATH,"//span[text()='My Account']").click()
+        self.driver.find_element(By.LINK_TEXT,"Login").click()
+        self.driver.find_element(By.ID,"input-email").send_keys("arun.motoori@gmail.com")
+        self.driver.find_element(By.ID,"input-password").send_keys("12345")
+        # driver.find_element(By.CLASS_NAME,"btn btn-primary").click()
+        self.driver.find_element(By.XPATH,"//input[@value='Login']").click()
+        assert self.driver.find_element(By.LINK_TEXT,"Edit your account information").is_displayed()
+        self.driver.quit()
 
+    def test_login_with_invalid_mailid_valid_password(self):
+        self.driver.find_element(By.XPATH,"//span[text()='My Account']").click()
+        self.driver.find_element(By.LINK_TEXT,"Login").click()
+        self.driver.find_element(By.ID,"input-email").send_keys("generate_email_with_timestamp()")
+        self.driver.find_element(By.ID,"input-password").send_keys("12345")
+        # driver.find_element(By.CLASS_NAME,"btn btn-primary").click()
+        self.driver.find_element(By.XPATH,"//input[@value='Login']").click()
+        expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
+        assert self.driver.find_element(By.XPATH,"//div[@id='account-login']/div[1]").text.__contains__(expected_warning_message)
+        self.driver.quit()
 
-def test_login_with_valid_credentials():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.XPATH,"//span[text()='My Account']").click()
-    driver.find_element(By.LINK_TEXT,"Login").click()
-    driver.find_element(By.ID,"input-email").send_keys("arun.motoori@gmail.com")
-    driver.find_element(By.ID,"input-password").send_keys("12345")
-    # driver.find_element(By.CLASS_NAME,"btn btn-primary").click()
-    driver.find_element(By.XPATH,"//input[@value='Login']").click()
-    assert driver.find_element(By.LINK_TEXT,"Edit your account information").is_displayed()
-    driver.quit()
+    def test_login_with_valid_mailid_invalid_password(self):
+        self.driver.find_element(By.XPATH, "//span[text()='My Account']").click()
+        self.driver.find_element(By.XPATH, "//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']").click()
+        self.driver.find_element(By.ID, "input-email").send_keys("arun.motoori@gmail.com")
+        self.driver.find_element(By.ID, "input-password").send_keys("1234567")
+        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
+        expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
+        assert self.driver.find_element(By.XPATH, "//div[@id='account-login']/div[1]").text.__contains__(
+            expected_warning_message)
+        self.driver.quit()
 
+    def test_login_without_entering_credentials(self):
+        self.driver.find_element(By.XPATH, "//span[text()='My Account']").click()
+        self.driver.find_element(By.XPATH, "//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']").click()
+        self.driver.find_element(By.ID, "input-email").send_keys()
+        self.driver.find_element(By.ID, "input-password").send_keys()
+        self.driver.find_element(By.XPATH, "//input[@value='Login']").click()
+        expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
+        assert self.driver.find_element(By.XPATH, "//div[@id='account-login']/div[1]").text.__contains__(
+            expected_warning_message)
+        self.driver.quit()
 
-def test_login_with_invalid_mailid_valid_password():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.XPATH,"//span[text()='My Account']").click()
-    driver.find_element(By.LINK_TEXT,"Login").click()
-    driver.find_element(By.ID,"input-email").send_keys("generate_email_with_timestamp()")
-    driver.find_element(By.ID,"input-password").send_keys("12345")
-    # driver.find_element(By.CLASS_NAME,"btn btn-primary").click()
-    driver.find_element(By.XPATH,"//input[@value='Login']").click()
-    expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
-    assert driver.find_element(By.XPATH,"//div[@id='account-login']/div[1]").text.__contains__(expected_warning_message)
-    driver.quit()
-
-
-def test_login_with_valid_mailid_invalid_password():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.XPATH, "//span[text()='My Account']").click()
-    driver.find_element(By.XPATH, "//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']").click()
-    driver.find_element(By.ID, "input-email").send_keys("arun.motoori@gmail.com")
-    driver.find_element(By.ID, "input-password").send_keys("1234567")
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
-    expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
-    assert driver.find_element(By.XPATH, "//div[@id='account-login']/div[1]").text.__contains__(
-        expected_warning_message)
-    driver.quit()
-
-
-def test_login_without_entering_credentials():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.XPATH, "//span[text()='My Account']").click()
-    driver.find_element(By.XPATH, "//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']").click()
-    driver.find_element(By.ID, "input-email").send_keys()
-    driver.find_element(By.ID, "input-password").send_keys()
-    driver.find_element(By.XPATH, "//input[@value='Login']").click()
-    expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
-    assert driver.find_element(By.XPATH, "//div[@id='account-login']/div[1]").text.__contains__(
-        expected_warning_message)
-    driver.quit()
-
-
-
-
-def generate_email_with_timestamp():
-    time_stamp = datetime.now().strftime("%y_%m_%d_%H_%M_%S")
-    return "amotoori"+time_stamp+"@gmail.com"
+    def generate_email_with_timestamp(self):
+        time_stamp = datetime.now().strftime("%y_%m_%d_%H_%M_%S")
+        return "amotoori"+time_stamp+"@gmail.com"
 
 
 
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,44 +1,26 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import pytest
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestSearch:
+    def test_search_for_a_valid_product(self):
+        self.driver.find_element(By.NAME,"search").send_keys("HP")
+        self.driver.find_element(By.XPATH,"//button[contains(@class,'btn-default')]").click()
+        assert self.driver.find_element(By.LINK_TEXT,"HP LP3065").is_displayed()
+        self.driver.quit()
 
+    def test_search_for_an_invalid_product(self):
+        self.driver.find_element(By.NAME,"search").send_keys("Honda")
+        self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
+        expected_text = "There is no product that matches the search criteria."
+        assert self.driver.find_element(By.XPATH,"//input[@id='button-search']/following-sibling::p").text.__eq__(expected_text)
+        self.driver.quit()
 
-def test_search_for_a_valid_product():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.NAME,"search").send_keys("HP")
-    driver.find_element(By.XPATH,"//button[contains(@class,'btn-default')]").click()
-
-    # Wait for the product link to be visible
-    WebDriverWait(driver, 30).until(
-        EC.visibility_of_element_located((By.LINK_TEXT, "HP LP3065"))
-    )
-    assert driver.find_element(By.LINK_TEXT,"HP LP3065").is_displayed()
-
-    driver.quit()
-
-
-def test_search_for_an_invalid_product():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.NAME,"search").send_keys("Honda")
-    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
-    expected_text = "There is no product that matches the search criteria."
-    assert driver.find_element(By.XPATH,"//input[@id='button-search']/following-sibling::p").text.__eq__(expected_text)
-    driver.quit()
-
-def test_search_without_entering_any_product_name():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://tutorialsninja.com/demo/")
-    driver.find_element(By.NAME, "search").send_keys()
-    driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
-    expected_text = "There is no product that matches the search criteria."
-    assert driver.find_element(By.XPATH, "//input[@id='button-search']/following-sibling::p").text.__eq__(expected_text)
-    driver.quit()
+    def test_search_without_entering_any_product_name(self):
+        self.driver.find_element(By.NAME, "search").send_keys()
+        self.driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
+        expected_text = "There is no product that matches the search criteria."
+        assert self.driver.find_element(By.XPATH, "//input[@id='button-search']/following-sibling::p").text.__eq__(expected_text)
+        self.driver.quit()
 
 
 
